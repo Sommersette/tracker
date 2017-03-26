8ec36b2ea8847438d5212ca2da4f1a3b157bb2ea
Trevor Fast <trevorrfast@gmail.com>
2017-03-25 17:36:25 -0700

    branch in survey to split between office size and java/android.

--- js/scripts.js
+++ js/scripts.js
@@ -12,25 +12,48 @@ $(document).ready(function(){
       $("#frontback").show();
       $("#screen").hide();
     } else {
-      $("#different ").show();
+      $("#different ").show(); // end of this branch
       $("#screen").hide();
     }
-
   });
 
   $("#frontback").submit(function(event) {
     event.preventDefault();
     var coding = $("input:radio[name=coding]:checked").val();
     if (coding === "frontend") {
-      $("#design").show();
+      $("#design").show(); // end of this branch
       $("#frontback").hide();
     } else {
-      $("#people").show();
+      $("#people").show(); // new branch
       $("#frontback").hide();
     }
-
-
 });
+
+  $("#people").submit(function(event) {
+    event.preventDefault();
+    var interaction = $("select").val();
+    if ("interaction === people") {
+      $("#office").show();
+      $("#people").hide();
+    } else {
+      $("#ruby").show();
+      $("#people").hide();
+  }
+});
+
+  $("#office").submit(function(event) {
+    event.preventDefault();
+    var workLocation = $("select").val();
+    if ("workLocation === office") {
+      $("#size").show();
+      $("#office").hide();
+    } else {
+      $("#android").show();
+      $("#office").hide();
+  }
+});
+
+
 
 
 
